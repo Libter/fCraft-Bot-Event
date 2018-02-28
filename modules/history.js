039a30156d949e8730544a6a38f875510afbddae
Libter <libter.pl@gmail.com>
2018-02-28 21:22:52 +0100

    Dodanie filtrowania historii!

--- modules/history.js
+++ modules/history.js
@@ -15,29 +15,55 @@ module.exports = async (message, args) => {
 
     for (const uuid in players) {
         const player = players[uuid];
-        history.push([player.applied, 'apply', uuid]);
+        const nick = resolved[uuid].replace('_', '\\_');
+        history.push([player.applied, 'apply', nick]);
         if (player.joined != null) {
-            history.push([player.joined, 'join', uuid]);
+            history.push([player.joined, 'join', nick]);
         }
         if (player.death.time != null) {
-            history.push([player.death.time, 'death', uuid, player.death.reason]);
+            history.push([player.death.time, 'death', nick, player.death.reason == null ? null : resolved[player.death.reason].replace('_', '\\_')]);
         }
     }
 
-    history.sort(function(a, b) {
+    const filters = args.slice(2);
+    history = history.filter(function(event) {
+        for (const i in filters) {
+            let filter = filters[i];
+            switch (filter) {
+                case 'zapisy':
+                    if (event[1] != 'apply') { return false; }
+                    break;
+                case 'dołączenia':
+                    if (event[1] != 'join') { return false; }
+                    break;
+                case 'śmierci':
+                    if (event[1] != 'death') { return false; }
+                    break;
+                default:
+                    filter = filter.replace('_', '\\_');
+                    if (filter != event[2] && filter != event[3]) { return false; }
+            }
+        }
+        return true;
+    });
+
+    history = history.sort(function(a, b) {
         return b[0] - a[0];
     });
 
-    const page = args[1] ? parseInt(args[1]) : 1;
-    if (page < 0) { page = 1; }
+    let page = args[1] ? parseInt(args[1]) : 1;
+    const maxPage = Math.ceil(history.length / 10);
+    if (page < 0 || isNaN(page)) { page = 1; }
+    if (page > maxPage) { page = maxPage; }
     const embed = new discord.RichEmbed();
     embed.setAuthor(`Historia eventu`, 'https://cdn.fcraft.pl/logo/event/v2.2.png');
     embed.setColor('FF8600');
+    let end = false;
     for (let i = (page - 1) * 10; i < page * 10; i++) {
         const event = history[i];
         if (event == undefined) { break; }
         const time = moment(event[0] * 1000).format('DD.MM.YYYY o HH:mm');
-        const nick = resolved[event[2]];
+        const nick = event[2];
         switch (event[1]) {
             case 'apply':
                 embed.addField(time, `Gracz ${nick} złożył podanie na serwer.`, false);
@@ -46,11 +72,11 @@ module.exports = async (message, args) => {
                 embed.addField(time, `Gracz ${nick} dołączył po raz pierwszy na serwer.`, false);
                 break;
             case 'death':
-                const reason = event[3] == null ? 'siły natury' : 'gracza ' + resolved[event[3]];
+                const reason = event[3] == null ? 'siły natury' : 'gracza ' + event[3];
                 embed.addField(time, `Gracz ${nick} został zabity przez ${reason}.`, false);
                 break;
         }
     }
-    embed.setFooter(`Strona ${page}, następna: event!historia ${page + 1}`)
+    embed.setFooter(`Strona ${page}/${maxPage}` + (page == maxPage ? '' : `, następna: event!historia ${page + 1}`))
     message.channel.send(embed);
 };
